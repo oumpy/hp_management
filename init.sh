61457fc8dcb6a56bc625244ceee8e5698f60cf97
AtamaokaC <realatamaokac@gmail.com>
2020-03-17 19:07:28 +0900

    init.sh updated to copy original theme files.

--- init.sh
+++ init.sh
@@ -3,4 +3,10 @@ git clone https://github.com/oumpy/hp_management.git
 cd hp_management
 git clone https://github.com/oumpy/oumpy.github.io.git ./output
 git submodule update -i
-cd themes && git submodule update voidy-bootstrap
+
+themename="voidy-bootstrap"
+cd themes
+git submodule update "$themename"
+cd ..
+mkdir -p "theme/$themename"
+cp -an "themes/$themename"/* "theme/$themename/"
