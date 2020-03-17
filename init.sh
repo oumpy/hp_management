9bae875f9ed79c7d6cedbe7e335898c02cb9df43
takyamamoto <41336057+takyamamoto@users.noreply.github.com>
2020-03-17 21:25:47 +0900

    Revert "Improvement of theme-related files/systems issues"

--- init.sh
+++ init.sh
@@ -3,10 +3,4 @@ git clone https://github.com/oumpy/hp_management.git
 cd hp_management
 git clone https://github.com/oumpy/oumpy.github.io.git ./output
 git submodule update -i
-
-themename="voidy-bootstrap"
-cd themes
-git submodule update "$themename"
-cd ..
-mkdir -p "theme/$themename"
-cp -an "themes/$themename"/* "theme/$themename/"
+cd themes && git submodule update voidy-bootstrap
