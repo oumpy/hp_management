7b44e62bacb24c3e03ca980bdaa2c301a3414d52
AtamaokaC <realatamaokac@gmail.com>
2020-03-24 02:03:18 +0900

    gotobranch.sh added; now blog_update.sh calls it.

--- blog_update.sh
+++ blog_update.sh
@@ -2,9 +2,5 @@
 comment="$1"
 branch=${2:-"master"}
 targetbranch=${3:-"master"}
-rm -rf content/*
-rm -rf theme/*
-git checkout -f $branch
-git pull origin $branch
-sh init.sh -c
+sh gotobranch.sh $branch
 sh blog_push.sh "$comment" $targetbranch
