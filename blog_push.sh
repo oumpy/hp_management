de468b3188745ea863f430ee4f995ec29a44bce6
AtamaokaC <realatamaokac@gmail.com>
2020-03-23 11:40:55 +0900

    blog_push.sh: further correction for safe.

--- blog_push.sh
+++ blog_push.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-cd output
-git pull origin master
-cd -
+cd output && git pull origin master && cd -
 make clean
 make publish
 cd output && git commit -a -m "Update" && git push origin master
