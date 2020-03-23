b0862fbb4e061c23d190b4360dd6e39f960adbda
AtamaokaC <realatamaokac@gmail.com>
2020-03-23 11:36:08 +0900

    blog_push.sh: delete old comments

--- blog_push.sh
+++ blog_push.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
-
-# https://qiita.com/driller/items/49a990cbdfb51afed620
-
-
-# git clone https://github.com/yuna06/chinese.git
-# cd english
-# pelican-quickstart
 cd output
 git pull origin master
 cd -
-# pelican content -o output -s pelicanconf.py
 make clean
 make publish
-#ghp-import output
-# git push origin gh-pages
-# git push https://github.com/oumpy/oumpy.github.io.git master
 cd output && git commit -a -m "Update" && git push origin master
