3bf2a5ff07472d8d640bb6cd16e3285fb7d35453
AtamaokaC <realatamaokac@gmail.com>
2020-03-24 01:53:38 +0900

    blog_update.sh: clean content & theme; co &then pull.

--- blog_update.sh
+++ blog_update.sh
@@ -2,7 +2,9 @@
 comment="$1"
 branch=${2:-"master"}
 targetbranch=${3:-"master"}
-git pull origin $branch
+rm -rf content/*
+rm -rf theme/*
 git checkout -f $branch
+git pull origin $branch
 sh init.sh -c
 sh blog_push.sh "$comment" $targetbranch
