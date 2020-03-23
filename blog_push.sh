cde2490b0041c7b2055e1958252bd042c90898bc
AtamaokaC <realatamaokac@gmail.com>
2020-03-23 12:52:02 +0900

    blog_push.sh: now commit comment can be given.

--- blog_push.sh
+++ blog_push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+comment="$1"
 cd output && git pull origin master && cd -
 make clean
 make publish
-cd output && git commit -a -m "Update" && git push origin master
+cd output && git commit -a -m ${comment:-"Update"} && git push origin master
