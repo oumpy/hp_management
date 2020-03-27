e33e403ebf2548ffc2fce0e6d81b2b4e1fa267f7
AtamaokaC <realatamaokac@gmail.com>
2020-03-27 17:02:16 +0900

    tools/init.sh: bugfix

--- tools/init.sh
+++ tools/init.sh
@@ -15,21 +15,21 @@ mkdir -p "theme/$themename"
 cp -an "themes/$themename"/* "theme/$themename/"
 
 # put webhook-cgi.
-cgitemplate=tools/deploy.py
+cgitemplate="tools/deploy.py"
 if [ -e "webhookconf.py" ]; then
     cgipath=`python3 -c "import os,sys; sys.path.append(os.curdir); import webhookconf; print(webhookconf.cgipath)"`
-    path=`python3 -c "import os,sys; sys.path.append(os.curdir); import webhookconf; print(webhookconf.path)"`
     if [ -n "$cgipath" ]; then
         if [ `echo "$cgipath" | rev | cut -c 1` == "/" ]; then
             cgipath="$cgipath`basename "$cgitemplate"`"
         fi
         pythonpath=`which python3`
         hpmanagement_path=`pwd`
+        path=`python3 -c "import os,sys; sys.path.append(os.curdir); import webhookconf; print(webhookconf.path)"`
         mkdir -p `dirname "$cgipath"`
+        cat "$cgitemplate" | \
         sed -e "s|\$pythonpath|$pythonpath|g" | \
         sed -e "s|\$hpmanagement_path|$hpmanagement_path|g" | \
         sed -e "s|\$path|$path|g" \
-            < $cgitemplate \
             > "$cgipath"
         chmod +x "$cgipath"
     fi
