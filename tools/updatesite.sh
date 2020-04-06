6d80ae22ae69e73cb196a95674e4c57cf4943e76
AtamaokaC <realatamaokac@gmail.com>
2020-04-06 18:05:03 +0900

    setcgi.sh was separated from init.sh

--- tools/updatesite.sh
+++ tools/updatesite.sh
@@ -3,4 +3,7 @@ comment="$1"
 branch=${2:-master}
 targetbranch=${3:-master}
 sh ./tools/gotobranch.sh $branch
+if [ "$branch"=="master" ]; then
+    sh ./tools/setcgi.sh
+fi
 sh ./tools/pushsite.sh "$comment" $targetbranch
