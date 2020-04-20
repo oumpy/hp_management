dbd6ae479b74aaa56581afae35aef9b8d0830a4b
AtamaokaC <realatamaokac@gmail.com>
2020-04-20 16:22:19 +0900

    Add a new plugin, CategoryNames.

---
+++ myplugins/category_names/category_names.py
@@ -0,0 +1,46 @@
+"""
+CategoryNames Plugin for Pelican
+-------
+
+This plugin replaces or adds alternative names for categories.
+"""
+
+from __future__ import unicode_literals
+from pelican import signals
+
+import logging
+logger = logging.getLogger(__name__)
+
+def initialize(instance):
+    from pelican.settings import DEFAULT_CONFIG
+    DEFAULT_CONFIG.setdefault('CATEGORYNAMES_ATTRIBUTES',
+                              ('name','name_long'))
+    DEFAULT_CONFIG.setdefault('CATEGORYNAMES_ALTERNATIVES',
+                              dict())
+    instance.settings.setdefault('CATEGORYNAMES_ATTRIBUTES',
+                                 ('name','name_long'))
+    instance.settings.setdefault('CATEGORYNAMES_ALTERNATIVES',
+                                 dict())
+
+
+def rename_category(category, settings):
+    attrs = settings['CATEGORYNAMES_ATTRIBUTES']    
+    alters = settings['CATEGORYNAMES_ALTERNATIVES']
+    cat = str(category)
+    category.slug = cat
+    if cat in alters.keys():
+        new_cats = alters[cat]
+        if not isinstance(new_cats,(list,tuple)):
+            new_cats = (new_cats,)
+        for i in range(min(len(attrs),len(new_cats))):
+            setattr(category, attrs[i], new_cats[i])
+
+
+def run_plugin(generator):
+    for article in generator.articles:
+        rename_category(article.category, generator.settings)
+
+
+def register():
+    signals.initialized.connect(initialize)
+    signals.article_generator_pretaxonomy.connect(run_plugin)
