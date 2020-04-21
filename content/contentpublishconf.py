8f9db8c4774aa20184298eeec030908a37352b07
AtamaokaC <realatamaokac@gmail.com>
2020-04-21 14:39:58 +0900

    Add sitemap.xml by using sitemap plugin.

--- content/contentpublishconf.py
+++ content/contentpublishconf.py
@@ -5,6 +5,21 @@ SITEREPOSITORY = 'https://github.com/oumpy/oumpy.github.io.git'
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'oumpy.github.io'
 
+# configuration for sitemap plugin
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'weekly',
+        'indexes': 'weekly',
+        'pages': 'weekly'
+    }
+}
+
 # Following items are often useful when publishing
 # Services
 # GOOGLE_ANALYTICS = 'UA-12345678-9'
