1811531742eec2c47016ee077da43d7fe17bd9e9
masssssa <masayuki.sugihara@milplateaux.com>
2022-06-03 15:13:49 +0900

    英語のカルテデータのコンペに参加してみた(際の失敗と反省)

--- content/create.py
+++ content/create.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# coding:utf-8
 from datetime import datetime
 from collections import defaultdict
 import os
@@ -10,13 +11,18 @@ template = "./template.md"
 today = datetime.now().strftime("%Y.%m.%d")
 fields = [  # (field, default, comment)
     ('slug',      'my_new_article',   'ファイル名。英数字、拡張子なし'),
-    ('date',      today,              '記事日付。YYYY.mm.DD または YYYY.mm.DD HH.MM 形式'),
+    ('date',      today,
+     '記事日付。YYYY.mm.DD または YYYY.mm.DD HH.MM 形式'),
     ('modified',  None,               '更新日。None の場合は date と同じ'),
-    ('title',     None,               '記事タイトル。content 内に # 要素がある場合は指定しない。'),
+    ('title',     None,
+     '記事タイトル。content 内に # 要素がある場合は指定しない。'),
     ('category',  'blog',             'blog, news または page'),
-    ('tags',      None,               'Bioinformatics など、サイト参照。複数の場合カンマ区切り'),
-    ('author',    None,               '記事執筆者の名前。姓またはHN、複数の場合カンマ区切り'),
-    ('content',   None,               '記事内容ファイル (.md/.ipynb) がすでにある場合はパスを指定'),
+    ('tags',      None,
+     'Bioinformatics など、サイト参照。複数の場合カンマ区切り'),
+    ('author',    None,
+     '記事執筆者の名前。姓またはHN、複数の場合カンマ区切り'),
+    ('content',   None,
+     '記事内容ファイル (.md/.ipynb) がすでにある場合はパスを指定'),
 ]
 
 if __name__ == '__main__':
@@ -25,12 +31,16 @@ if __name__ == '__main__':
     shortused = defaultdict(bool)
     for field, default, comment in fields:
         if shortused[field[0].lower()]:
-            parser.add_argument('--{}'.format(field.lower()), default=None, help=comment)
+            parser.add_argument('--{}'.format(field.lower()),
+                                default=None, help=comment)
         else:
-            parser.add_argument('--{}'.format(field.lower()), '-{}'.format(field[0].lower()), default=None, help=comment)
+            parser.add_argument('--{}'.format(field.lower()),
+                                '-{}'.format(field[0].lower()), default=None, help=comment)
             shortused[field[0].lower()] = True
-    parser.add_argument('--filename', '-f', default=None, help='--slug と同じ。両方指定すると slug が優先')
-    parser.add_argument('--default', action='store_true', default=None, help='未指定の項目を質問せずデフォルト値にする')
+    parser.add_argument('--filename', '-f', default=None,
+                        help='--slug と同じ。両方指定すると slug が優先')
+    parser.add_argument('--default', action='store_true',
+                        default=None, help='未指定の項目を質問せずデフォルト値にする')
 
     args = parser.parse_args()
     args.slug = args.slug or args.filename
@@ -47,10 +57,12 @@ if __name__ == '__main__':
     for field, default, comment in fields:
         Field = field.capitalize()
         if args.default or getattr(args, field):
-            print('{0}: {1}'.format(Field, getattr(args, field) or fields[field][1]))
+            print('{0}: {1}'.format(Field, getattr(
+                args, field) or fields[field][1]))
             values[field] = getattr(args, field)
         else:
-            print('{0} ({2}) [ {1} ]: '.format(Field, default, comment), end='')
+            print('{0} ({2}) [ {1} ]: '.format(
+                Field, default, comment), end='')
             given = input().strip()
             if given:
                 values[field] = given
@@ -69,7 +81,8 @@ if __name__ == '__main__':
     if values['category'].lower() in {'page', 'pages'}:
         directory = './pages'
     else:
-        directory = "./articles/{0}sy/{1}".format(schoolyear, values['category'])
+        directory = "./articles/{0}sy/{1}".format(
+            schoolyear, values['category'])
     filepath = "{0}/{1}.md".format(directory, values['slug'])
     imagedirpath = "{0}/images/{1}_figs".format(directory, values['slug'])
 
@@ -88,7 +101,7 @@ if __name__ == '__main__':
                     for line in cf.readlines():
                         line = line.rstrip('\n')
                         if not values['title']:
-                            if line and line[0]=='#':
+                            if line and line[0] == '#':
                                 values['title'] = line.lstrip('#').strip()
                                 continue
                         content.append(line)
@@ -100,14 +113,17 @@ if __name__ == '__main__':
                     print("'{}' はすでに存在します。".format(filepath))
                 else:
                     shutil.copy(contentfile, filepath_ipynb)
-                    print("ファイル '{}' を作成 (コピー) しました。".format(filepath_ipynb))
+                    print("ファイル '{}' を作成 (コピー) しました。".format(
+                        filepath_ipynb))
             else:
-                print("ファイル '{}' は対応するファイル形式ではありません。指定を無視します。".format(contentfile))
+                print("ファイル '{}' は対応するファイル形式ではありません。指定を無視します。".format(
+                    contentfile))
         else:
-            print("ファイル '{}' は存在しません。指定を無視します。".format(contentfile))
+            print("ファイル '{}' は存在しません。指定を無視します。".format(
+                contentfile))
 
     if os.path.exists(filepath):
-    # file already exists ?
+        # file already exists ?
         print("Error: '{}' はすでに存在します。".format(filepath))
         exit()
 
@@ -125,7 +141,8 @@ if __name__ == '__main__':
                     if field in {'author', 'tags'}:
                         values[field] = values[field].strip().strip(',')
                     if field == 'author' and len(values[field].split(',')) > 1:
-                        print('{0}s: {1}'.format(Field, values[field]), file=tf)
+                        print('{0}s: {1}'.format(
+                            Field, values[field]), file=tf)
                     else:
                         print('{0}: {1}'.format(Field, values[field]), file=tf)
                 elif suffix == 'md' or line.strip():
@@ -137,9 +154,12 @@ if __name__ == '__main__':
     if suffix == '.md':
         try:
             os.makedirs(imagedirpath)
-            print("画像保存用ディレクトリ'{}'を作成しました。".format(imagedirpath))
+            print("画像保存用ディレクトリ'{}'を作成しました。".format(
+                imagedirpath))
         except FileExistsError:
             if os.path.isdir(imagedirpath):
-                print("画像保存用ディレクトリ'{}'はすでに存在します。".format(imagedirpath))
+                print("画像保存用ディレクトリ'{}'はすでに存在します。".format(
+                    imagedirpath))
             else:
-                print("画像保存用ディレクトリ'{}'を作成できませんでした。".format(imagedirpath))
+                print("画像保存用ディレクトリ'{}'を作成できませんでした。".format(
+                    imagedirpath))
