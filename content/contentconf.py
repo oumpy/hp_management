9571a1d2a0a230128fa4a23d045d99d1bd0c1a87
AtamaokaC <realatamaokac@gmail.com>
2026-08-11 06:02:47 +0000

    Fix social sidebar: stray underscores and Connpass icon (official logo)
    Two fixes for the sidebar 'ソーシャル' box:
    
    - The underscores between icons were underlined whitespace inside the
      <a> elements: Bootstrap 5 underlines links by default (BS4 did not),
      which exposed both the stray newlines in the sb_social template and
      a long-standing unclosed </span> typo.  Tighten the template
      whitespace, fix the typo, and disable underlining for icon links
      (sidebar box text links keep underline on hover).  The visual gap
      the whitespace used to provide is now an explicit margin
      (li:not(:last-child) { margin-right: .35em }).
    - The Connpass icon hotlinked connpass.com/static/img/72_72.png, which
      disappeared in the connpass site renewal.  Embed the official
      connpass_logo_3.png distributed on the help page
      'ロゴ画像を使いたい' (https://help.connpass.com/faq/others.html,
      resize-only), with CSS to render it inline at icon height.  If the
      image ever disappears again the alt text 'connpass' is shown, so
      breakage is at least visible.  Variants _1 to _4 can be swapped by
      number.

--- content/contentconf.py
+++ content/contentconf.py
@@ -37,7 +37,11 @@ SOCIAL = [ # (name, URL, icon, color, size)
     ('E-mail', 'mailto:handai.python@gmail.com', '<i class="far fa-envelope"></i>', '#0078D4', 'larger'),
     ('GitHub Organization', 'https://github.com/oumpy', '<i class="fab fa-github"></i>', '#211F1F', 'larger'),
     ('YouTube Channel', 'https://www.youtube.com/channel/UCh1eAeDCpsZeOh0Z9paNfHQ', '<i class="fab fa-youtube"></i>', '#c4302b', 'larger'),
-    ('Connpass', 'https://oum-python.connpass.com', '<img width="35px" src="https://connpass.com/static/img/72_72.png" style="display: inline;"/>', '#000000', 'normal'),
+    # connpass公式ロゴ。配布元: https://help.connpass.com/faq/others.html
+    # (「ロゴ画像を使いたい」節。リサイズ以外の加工は不可)
+    # 旧URL (connpass.com/static/img/72_72.png) はサイトリニューアルで消滅した。
+    # 画像が読めない場合は alt テキストが表示される。ロゴ変種は _1〜_4。
+    ('Connpass', 'https://oum-python.connpass.com', '<img class="social-logo-image" src="https://help.connpass.com/_images/connpass_logo_3.png" alt="connpass"/>', '#E9463F', 'normal'),
     ('Atom Feed', './feeds/all.atom.xml', '<i class="fa fa-rss fa-fw fa-lg"></i>', '#00008b', 'smaller'),
     ('RSS Feed', './feeds/all.rss.xml', '<i class="fas fa-rss-square fa-fw fa-lg"></i>', '#f26522', 'normal'),
 ]
